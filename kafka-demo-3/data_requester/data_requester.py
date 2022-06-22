cef0ec455e5a70601fed75a1af146050c3da2ac2
InHwan Jung <inhwan.jung@gmail.com>
2022-06-21 20:19:35 -0700

    2022_06_21_08_19_PM

--- kafka-demo-3/data_requester/data_requester.py
+++ kafka-demo-3/data_requester/data_requester.py
@@ -1,27 +1,39 @@
+import configparser
 import faust
 import json
 import uuid
 
-class Order(faust.Record):
+config = configparser.ConfigParser()
+config.read('config.ini', encoding='utf-8') 
+
+TOPIC=config['kafka']['topic']
+SERVER=config['kafka']['server'].split(',')
+REGION=config['command']['region']
+AZ=config['command']['az']
+COMMAND=config['command']['command']
+
+print(SERVER)
+
+class REQUEST(faust.Record):
     region: str
     az: str
     uuid: str
     command: str
 
-app = faust.App('order', 
-                broker=['10.11.65.187:9092','10.11.65.187:9093','10.11.65.187:9094'],
+app = faust.App(TOPIC, 
+                broker=SERVER,
                 store='memory://',
 )
 
-topic = app.topic("orders", value_type=Order)
+topic = app.topic(TOPIC, value_type=REQUEST)
 
 # Producer
 @app.timer(interval=1.0)
-async def order_sender(app):
-    order = Order(region='kr', az='kr1', uuid = uuid.uuid1(), command='ls -al')
+async def requester(app):
+    command_request = REQUEST(region=REGION, az=AZ, uuid = uuid.uuid1(), command=COMMAND)
 
     await topic.send(
-        value = order,
+        value = command_request,
     )
 
 if __name__ == "__main__":
