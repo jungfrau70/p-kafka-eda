7068bc5b263cd814fe94ac4328f8216c098bdc10
InHwan Jung <inhwan.jung@gmail.com>
2022-07-13 00:51:06 -0700

    2022_07_13_12_51_AM

--- kafka-demo-3/relay-k8s-metrics-to-kr1-prd/relay-k8s-metrics-to-kr1-prd.py
+++ kafka-demo-3/relay-k8s-metrics-to-kr1-prd/relay-k8s-metrics-to-kr1-prd.py
@@ -2,18 +2,20 @@
 from kafka import KafkaConsumer, KafkaProducer
 import json 
 
-KR1_PRD_TOPIC = "k8s-metrics"
+KR1_PRD_TOPIC = "kr-1-stg-k8s-metrics"
 KR1_STG_TOPIC = "k8s-metrics"
 
-# kr1_prd_brokers = ['192.168.171.132:9092','192.168.171.132:9093','192.168.171.132:9094'] # office
-# kr1_stg_brokers = ['192.168.171.132:9092','192.168.171.132:9093','192.168.171.132:9094'] # office
+kr1_prd_brokers = ['192.168.171.132:9092','192.168.171.132:9093','192.168.171.132:9094'] # office
+kr1_stg_brokers = ['192.168.171.132:9092','192.168.171.132:9093','192.168.171.132:9094'] # office
 
-kr1_prd_brokers = ['10.11.65.187:9092','10.11.65.187:9093','10.11.65.187:9094'] # private cloud
-kr1_stg_brokers = ['10.11.76.177:9092','10.11.76.177:9093','10.11.76.177:9094'] # private cloud
+# kr1_prd_brokers = ['10.11.65.187:9092','10.11.65.187:9093','10.11.65.187:9094'] # private cloud
+# kr1_stg_brokers = ['10.11.76.177:9092','10.11.76.177:9093','10.11.76.177:9094'] # private cloud
 
 consumer = KafkaConsumer(KR1_STG_TOPIC, bootstrap_servers=kr1_stg_brokers)
 producer = KafkaProducer(bootstrap_servers=kr1_prd_brokers)
 
 for message in consumer:
-  msg = json.loads(message.value.decode())
-  producer.send(KR1_PRD_TOPIC, json.dumps(msg).encode("utf-8"))
+  producer.send(KR1_PRD_TOPIC, message.value)  
+
+  # msg = json.loads(message.value.decode())
+  # producer.send(KR1_PRD_TOPIC, json.dumps(msg).encode("utf-8"))
