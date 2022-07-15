1babf08148120e66b8d0c0e69d5c4a182fa2a2d9
InHwan Jung <inhwan.jung@gmail.com>
2022-07-15 01:41:44 -0700

    2022_07_15_01_41_AM

--- kafka-demo-3/relay-k8s-metrics-to-hq-prd/relay-k8s-metrics-to-hq-prd.py
+++ kafka-demo-3/relay-k8s-metrics-to-hq-prd/relay-k8s-metrics-to-hq-prd.py
@@ -8,11 +8,11 @@ KR1_STG_TOPIC = "kr-1-stg-k8s-metrics"
 # HQ_brokers = ['192.168.171.132:9092','192.168.171.132:9093','192.168.171.132:9094'] # office
 # kr1_stg_brokers = ['192.168.171.132:9092','192.168.171.132:9093','192.168.171.132:9094'] # office
 
-HQ_brokers = ['10.11.65.187:9092','10.11.65.187:9093','10.11.65.187:9094'] # private cloud
+hq_brokers = ['10.11.65.187:9092','10.11.65.187:9093','10.11.65.187:9094'] # private cloud
 kr1_stg_brokers = ['10.11.76.177:9092','10.11.76.177:9093','10.11.76.177:9094'] # private cloud
 
 consumer = KafkaConsumer(KR1_STG_TOPIC, bootstrap_servers=kr1_stg_brokers)
-producer = KafkaProducer(bootstrap_servers=HQ_brokers)
+producer = KafkaProducer(bootstrap_servers=hq_brokers)
 
 for message in consumer:
   producer.send(HQ_TOPIC, message.value)  
